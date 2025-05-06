77a150c25e964087a686250a88b6eb164d2f2855
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-05-06 14:07:59 -0300

    Update

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -10,8 +11,22 @@ import (
 )
 
 func main() {
-	tempFile := "temp/main.un"
-	file, err := os.Open(tempFile)
+	inputPath := flag.String("i", "", "input file")
+	dump := flag.Bool("dump", false, "dump parsed instructions")
+	repl := flag.Bool("repl", false, "run interactive REPL")
+	flag.Parse()
+
+	if *repl {
+		startRepl()
+		return
+	}
+
+	if len(*inputPath) == 0 {
+		log.Fatalln("ERROR: Not input file")
+		return
+	}
+
+	file, err := os.Open(*inputPath)
 	if err != nil {
 		log.Fatalln("ERROR:", err)
 		return
@@ -37,8 +52,49 @@ func main() {
 		log.Fatalln("ERROR:", err)
 		return
 	}
-	program.dump()
+
+	if *dump {
+		program.dump()
+	}
+
 	program.run()
+}
+
+func startRepl() {
+	fmt.Println("Interactive REPL. Type `exit` to quit.")
+	program := Program{}
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			break
+		}
+		line := strings.TrimSpace(scanner.Text())
+		if line == "exit" {
+			break
+		}
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+		tokens := strings.Split(line, " ")
+		err := program.Parse(tokens)
+		if err != nil {
+			fmt.Println("ERROR:", err)
+			continue
+		}
+		stack := program.run()
+		if len(stack) > 0 {
+			fmt.Printf(":- ")
+			for i, n := range stack {
+				if i > 0 {
+					fmt.Printf(", ")
+				}
+				fmt.Printf("%v", n)
+			}
+			fmt.Printf("\n")
+		}
+	}
 }
 
 type Program []ParsedLine
@@ -52,7 +108,7 @@ func (program Program) dump() {
 	}
 }
 
-func (program Program) run() {
+func (program Program) run() []int64 {
 	stack := []int64{}
 	for _, pl := range program {
 		for _, ins := range pl.instrs {
@@ -61,7 +117,7 @@ func (program Program) run() {
 				stack = append(stack, ins.operand)
 			case PLUS:
 				if len(stack) < 2 {
-					log.Fatalln("ERROR: not enough values on stack for '+'")
+					log.Fatalf("ERROR: not enough values on stack for '%s'\n", ins.kind.String())
 				}
 				b := stack[len(stack)-1]
 				a := stack[len(stack)-2]
@@ -75,11 +131,12 @@ func (program Program) run() {
 				stack = stack[0 : len(stack)-1]
 				println(a)
 			default:
-				log.Fatalln("ERROR: unreachable")
-				return
+				log.Fatalln("ERROR: unimplemented", ins.kind.String())
+				return stack
 			}
 		}
 	}
+	return stack
 }
 
 func (p *Program) Parse(lineTokens []string) error {
@@ -93,6 +150,10 @@ func (p *Program) Parse(lineTokens []string) error {
 		case "+":
 			{
 				pl.instrs = append(pl.instrs, Instr{PLUS, 0})
+			}
+		case "-":
+			{
+				pl.instrs = append(pl.instrs, Instr{MINUS, 0})
 			}
 		default:
 			{
@@ -129,16 +190,45 @@ func (ins Instr) String() string {
 
 type InstrKind uint
 
+const (
+	PUSH_INT InstrKind = iota
+	PLUS
+	MINUS
+	PRINT
+)
+
+func (ins InstrKind) CheckArity(stack_size int) bool {
+	n, _ := ins.Arity()
+	if stack_size < n {
+		log.Fatalf("ERROR: not enough values on stack for '%s'\n", ins.String())
+	}
+}
+
+func (ins InstrKind) Arity() (int, int) {
+	switch ins {
+	case PLUS, MINUS:
+		return 2, 1
+	case PRINT:
+		return 1, 0
+	case PUSH_INT:
+		return 0, 1
+	default:
+		panic("Unknown InstrKind")
+	}
+}
+
 func (ins InstrKind) String() string {
 	switch ins {
 	case PUSH_INT:
 		return "push_int"
 	case PLUS:
 		return "+"
+	case MINUS:
+		return "-"
 	case PRINT:
 		return "print"
 	default:
-		return "Unknown"
+		panic("Unknown InstrKind")
 	}
 }
 
@@ -146,17 +236,7 @@ func (ins InstrKind) HasOperand() bool {
 	switch ins {
 	case PUSH_INT:
 		return true
-	case PLUS:
-		return false
-	case PRINT:
-		return false
 	default:
 		return false
 	}
 }
-
-const (
-	PUSH_INT InstrKind = iota
-	PLUS
-	PRINT
-)
